da4fac4985032e69d6ee9bc763ea9f70a9c75a34
maRk <mark45u.u4gm2006@gmail.com>
2012-05-31 08:23:20 +0900

    Add to include config_value.

--- php/modifier.TwUserLink.php
+++ php/modifier.TwUserLink.php
@@ -8,11 +8,21 @@ function smarty_modifier_TwUserLink( $str, $args) {
 		, $str);
 }
 function twlink($matches){
+		$mt = MT::get_instance();
+		$ctx =& $mt->context();
+		$blog_id = 'blog:' . $ctx->stash('blog_id');
+
 	if ($matches[1]){
 		$line = '@' . $matches[2];
 	}
 	else {
-		$line = '<a class="twitter-anywhere-user" href="//www.twitter.com/' .$matches[2]. '">'  .'@'. $matches[2] . '</a>';
+		$conf = $mt->db()->fetch_plugin_data ( 'TwUserLinky', 'configuration:' . $blog_id );
+		$link_target = $conf['twLinky_target'];
+		$link_class = $conf['twLinky_class'];
+		$target = $link_target ==='1' ? ' target="_blank"' : '';
+		$class  = $link_class ==='1' ? ' class="twitter-anywhere-user"' : '';
+
+		$line = '<a' . $class . ' href="//www.twitter.com/' .$matches[2]. '"' . $target . '>' .'@'. $matches[2] . '</a>';
 	}
 return $line;
 }
