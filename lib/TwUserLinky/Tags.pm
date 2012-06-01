64235c5f3feae8e78d6382eadfd03d85518b0d45
maRk <mark45u.u4gm2006@gmail.com>
2012-06-01 17:05:39 +0900

    A little change

--- lib/TwUserLinky/Tags.pm
+++ lib/TwUserLinky/Tags.pm
@@ -12,7 +12,7 @@ sub _hdlr_tw_user_link{
 		my $blog_id = $ctx->stash('blog_id');
 		my $link_target = $plugin->get_config_value('twLinky_target', 'blog:'.$blog_id);
 		my $link_class =  $plugin->get_config_value('twLinky_class', 'blog:'.$blog_id);
-		$str =~ s/([\.\\])?\@(\w{1,15})/&tw_link($1, $2, $link_target, $link_class)/ieg;
+		$str =~ s/([\.\\])?\@(\w{1,15})/&tw_link($1, $2, $link_target, $link_class)/eg;
 		return $str;
 }
 
