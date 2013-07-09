734273ee9d9b742a20b2f8be8e41ed6e084a9364
maRk <mark45u.u4gm2006@gmail.com>
2013-07-09 21:47:02 +0900

    added configuration of space to between words

--- php/modifier.TwUserLink.php
+++ php/modifier.TwUserLink.php
@@ -1,28 +1,35 @@
-<?php 
-function smarty_modifier_TwUserLink( $str, $args) {
-	if ($args != 1) {
-		return $str;
-	}
-	return  preg_replace_callback('/([\\.\\\])?@([0-9a-zA-Z_]{1,15})/'
-		, twlink
-		, $str);
-}
-function twlink($matches){
-		$mt = empty($mt)? (MT::get_instance()) : $mt;
-		$ctx =& $mt->context();
-		$blog_id = 'blog:' . $ctx->stash('blog_id');
-		$conf = empty($conf)? $mt->db()->fetch_plugin_data ( 'TwUserLinky', 'configuration:' . $blog_id ) : $conf;
-		$link_target = $conf['twLinky_target'];
-		$link_class = $conf['twLinky_class'];
-
-	if ($matches[1]){
-		$line = '@' . $matches[2];
-	}
-	else {
-		$target = $link_target ==='1' ? ' target="_blank"' : '';
-		$class  = $link_class ==='1' ? ' class="twitter-anywhere-user"' : '';
-
-		$line = '<a' . $class . ' href="https://twitter.com/' .$matches[2]. '"' . $target . '>' .'@'. $matches[2] . '</a>';
-	}
-return $line;
-}
+<?php
+    function smarty_modifier_twuserlink ( $str, $args ){
+        if ($args != 1) {
+            return $str;
+        }
+        return  preg_replace_callback('/([\\.\\\])?@([0-9a-zA-Z_]{1,15})/', twlink, $str);
+    }
+    function twlink($matches){
+        
+        $conf_file = 'mt-config.cgi';
+        $mt = MT::get_instance(null, $conf_file);
+        $ctx =& $mt->context();
+        $blog_id = 'blog:' . $ctx->stash('blog_id');
+        $conf = array();
+        $conf = $mt->db()->fetch_plugin_config ( 'TwUserLinky' , $blog_id );
+        
+        $add_segmentation = $conf["twLinky_seg"];
+        $link_target = $conf["twLinky_target"];
+        $link_class = $conf["twLinky_class"];
+        
+        if ($matches[1]){
+            $line = '@' . $matches[2];
+        }
+        else {
+            $line = '';
+            $target = $link_target === "true" ? ' target="_blank"' : '';
+            $class  = $link_class === "true" ? ' class="twitter-anywhere-user"' : '';
+            $space  = $add_segmentation === "true" ? ' ' : '';
+            $data = ' data-blogid="'.$blog_id.'"'. ' data-target="'.$link_target. '" data-seg="'.$add_segmentation. '"';
+            $line .= $space . '<a'  . $data . $class . ' href="https://twitter.com/' .$matches[2]. '"' . $target . '>' .'@'. $matches[2] . '</a>' . $space;
+        }
+        
+        return $line;
+    }
+?>
